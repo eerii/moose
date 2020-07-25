97707d9f67e80b0bf261c81239ebf0e9cc609ae6
José Pazos Pérez <jose.pazos.perez@rai.usc.es>
2020-07-25 17:58:28 +0200

    Fix CORS 3

--- db.js
+++ db.js
@@ -20,16 +20,22 @@ const connect = () => {
 module.exports.add = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false
 
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+    }
+
     connect()
         .then(() => {
             BetaUser.create(JSON.parse(event.body))
                 .then(user => callback(null, {
                     statusCode: 200,
+                    headers,
                     body: JSON.stringify(user)
                 }))
                 .catch(err => callback(null, {
                     statusCode: err.statusCode || 500,
-                    headers: { 'Content-Type': 'text/plain' },
+                    headers,
                     body: 'Could not create the user entry.'
                 }))
         })
